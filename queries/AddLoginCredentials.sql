27beb38ce1e5a4959ff202d9cda5ea8a9fe71a96
Rob Tierolff <teh@borreh.nl>
2015-10-24 02:14:48 +0200

    Revised Login-creator to use left instead of substring

--- queries/AddLoginCredentials.sql
+++ queries/AddLoginCredentials.sql
@@ -1,12 +1,8 @@
 INSERT INTO Login(username, password, salt)
 VALUES (
 	@username, 
-	SHA2(
-		CONCAT(
-			@password, 
-			SUBSTRING(@session, 0, 20)), 
-		512), 
-	SUBSTRING(@session, 0, 20)
+	SHA2(CONCAT(@password, LEFT(@sessioncookie, 20)), 512), 
+	LEFT(@sessioncookie, 20)
 );
 
 SELECT LAST_INSERT_ID() login;
