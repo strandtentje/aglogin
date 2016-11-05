98dd9ae393397e4e8f88b02c32b7fad59bdabe3a
Rob Tierolff <teh@borreh.nl>
2016-11-05 22:57:13 +0100

    Made things more rational

--- queries/GetSession.sql
+++ queries/GetSession.sql
@@ -1,8 +1,11 @@
 SELECT
 	id session,
-	login
+	login,
+	(NOW() BETWEEN Session.begin AND IFNULL(Session.end, '9999-12-31')) AS isvalid	
 FROM
 	Session
 WHERE
-	cookie = @sessioncookie AND NOW() BETWEEN Session.begin AND IFNULL(Session.end, '9999-12-31')
-LIMIT 1;
+	cookie = @sessioncookie
+ORDER BY 
+	(NOW() BETWEEN Session.begin AND IFNULL(Session.end, '9999-12-31'))
+LIMIT 1
